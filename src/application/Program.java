fd5fbd7a8d170e66aff624c8c45efb44319e2cb9
henrique <henrique@teste.com>
2025-05-28 13:42:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
+		Department dep = new Department(1, "Computers");		
+		Seller sel = new Seller(12, "Fulano", "fulano@gmail.com", new Date(), 3000.00, dep);
 		
-
+		System.out.println(sel);
 	}
 
 }
